b6282f6989aa2c71fd8bad77200aa43fd9a10c76
JATS1986 <aldo.uag@gmail.com>
2024-07-12 09:54:52 -0300

    Atualização com os comentários

--- controle-de-fluxo/src/Contador.java
+++ controle-de-fluxo/src/Contador.java
@@ -4,9 +4,9 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
+        int parametroUm = terminal.nextInt(); //Lê o primeiro número inteiro do usuário
         System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+        int parametroDois = terminal.nextInt(); //Lê o segundo número inteiro do usuário
 
         try {
 
@@ -16,16 +16,17 @@ public class Contador {
 
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
+            
         } catch (ParametrosInvalidosException e) {
             System.out.println(e.getMessage());
         }
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-
         int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
-
+		
+        //realizar o for para imprimir os números com base na variável contagem
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
